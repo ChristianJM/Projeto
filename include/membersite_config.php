983b19b961363022a59d631ee00cf3f742e3ec2b
ChristianJM <christian.j.maruqes@hotmail.com>
2015-07-17 02:43:33 +0100

    jbjbknknk

---
+++ include/membersite_config.php
@@ -0,0 +1,26 @@
+<?PHP
+require_once("./include/fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Provide your site name here
+$fgmembersite->SetWebsiteName('localhost/source');
+
+//Provide the email address where you want to get notifications
+$fgmembersite->SetAdminEmail('christian.j.marques@hotmail.com');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, fgusers in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*username*/'Christian',
+                      /*password*/'ed123456',
+                      /*database name*/'mgpro',
+                      /*table name*/'usuarios');
+
+//For better security. Get a random string from this link: http://tinyurl.com/randstr
+// and put it here
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+?>
